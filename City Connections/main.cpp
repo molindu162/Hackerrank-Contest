1918c87522362a642c159df90b5053f2b6c00e18
molindu162 <molinduachintha24@gmail.com>
2023-06-09 14:37:57 +0530

    commit

--- City Connections/main.cpp
+++ City Connections/main.cpp
@@ -3,9 +3,41 @@
 
 using namespace std;
 
-void DFS(vector<vector<int> > edges, vector<bool> &visited, int start, int end, bool &isCyclic, int &paths)
+bool isCyclicUtil(vector<vector<int> > edges, vector<bool> visited, int curr){
+    if (visited[curr] == true){
+        return true;
+    }
+
+    visited[curr] = true;
+    bool FLAG = false;
+    for (int i = 0; i < edges[curr].size(); i++){
+        FLAG = isCyclicUtil(edges, visited, edges[curr][i]);
+        if (FLAG == true){
+            return true;
+        }
+    }
+
+    return false;
+}
+
+bool isCyclic(int V, vector<vector<int> > edges){
+    vector<bool> visited(V, false);
+    bool FLAG = false;
+    for (int i = 0; i < V; i++){
+        visited[i] = true;
+        for (int j = 0; j < edges[i].size(); j++){
+            FLAG = isCyclicUtil(edges, visited, edges[i][j]);
+            if (FLAG == true){
+                return true;
+            }
+        }
+        return false;
+    }
+}
+
+void DFS(vector<vector<int> > edges, vector<bool> &visited, int start, int end, int &paths)
 {
-    if (isCyclic == true){
+    if (isCyclic(5, edges) == true){
         paths = INT_MAX;
         return;
     }
@@ -21,12 +53,13 @@ void DFS(vector<vector<int> > edges, vector<bool> &visited, int start, int end, 
     for (int neighbour: neighbours)
     {
         if (visited[neighbour] == false) {
-            DFS(edges, visited, neighbour, end, isCyclic, paths);
+            DFS(edges, visited, neighbour, end, paths);
         }
       else{
-          isCyclic = true;
-          paths = INT_MAX;
-          return;
+          if (isCyclic(5, edges)){
+              paths = INT_MAX;
+              return;
+          }
       }
 
     }
@@ -37,7 +70,7 @@ int countPaths(int n, vector<vector<int> > edges)
     vector<bool> visited(5, false);
     bool isCyclic = false;
     int paths = 0;
-    DFS(edges, visited, 0, 4, isCyclic, paths);
+    DFS(edges, visited, 0, 4, paths);
     return paths;
 
 }
@@ -45,16 +78,18 @@ int countPaths(int n, vector<vector<int> > edges)
 int main()
 {
     vector<int> a;
-    a.push_back(1);
     a.push_back(2);
+    a.push_back(3);
+
 
     vector<int> b;
-    b.push_back(3);
+    b.push_back(0);
 
     vector<int> c;
     c.push_back(4);
 
     vector<int> d;
+    d.push_back(1);
     d.push_back(4);
 
     vector<int> e;
