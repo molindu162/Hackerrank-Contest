066557a2214e216d05b5d4b24bc7ec128bb70ab9
molindu162 <molinduachintha24@gmail.com>
2023-06-09 11:05:25 +0530

    changed

--- City Connections/main.cpp
+++ City Connections/main.cpp
@@ -3,15 +3,26 @@
 
 using namespace std;
 
-void DFS(vector<vector<int> > edges, vector<bool> visited, int start)
+void DFS(vector<vector<int> > edges, vector<bool> visited, int start, bool isCyclic)
 {
-
+    if (isCyclic == true){
+        return;
+    }
+    visited[start] = true;
     vector<int> neighbours;
+    for (int neighbour: neighbours)
+    {
+        if (visited[neighbour] == false){
+            visited[neighbour] = true;
+        }
+    }
 }
 
 void countPaths(int n, vector<vector<int> > edges)
 {
     vector<bool> visited(false, n);
+    bool isCyclic = false;
+
 }
 
 int main()
