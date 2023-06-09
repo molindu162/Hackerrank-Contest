39bce369937d94372d0337d9438c432b4ab1308b
molindu162 <molinduachintha24@gmail.com>
2023-06-09 11:19:12 +0530

    changed

--- City Connections/main.cpp
+++ City Connections/main.cpp
@@ -3,29 +3,67 @@
 
 using namespace std;
 
-void DFS(vector<vector<int> > edges, vector<bool> visited, int start, bool isCyclic)
+void DFS(vector<vector<int> > edges, vector<bool> &visited, int start, int end, bool &isCyclic, int &paths)
 {
     if (isCyclic == true){
+        paths = INT_MAX;
         return;
     }
+
+    if (start == end)
+        paths += 1;
+
     visited[start] = true;
-    vector<int> neighbours;
+    vector<int> neighbours = edges[start];
     for (int neighbour: neighbours)
     {
         if (visited[neighbour] == false){
             visited[neighbour] = true;
+            DFS(edges, visited, neighbour, end, isCyclic, paths);
+        }
+        else{
+            isCyclic = true;
+            paths = INT_MAX;
+            return;
         }
     }
 }
 
-void countPaths(int n, vector<vector<int> > edges)
+int countPaths(int n, vector<vector<int> > edges)
 {
     vector<bool> visited(false, n);
     bool isCyclic = false;
-
+    int paths = 0;
+    DFS(edges, visited, 0, 4, isCyclic, paths);
+    return paths;
 }
 
 int main()
 {
+    vector<int> a;
+    a.push_back(1);
+    a.push_back(2);
+
+    vector<int> b;
+    a.push_back(3);
+
+    vector<int> c;
+    a.push_back(4);
+
+    vector<int> d;
+    a.push_back(4);
+
+    vector<int> e;
+
+    vector<vector<int> > edges;
+    edges.push_back(a);
+    edges.push_back(b);
+    edges.push_back(c);
+    edges.push_back(d);
+    edges.push_back(e);
+
+    int noOfPaths = countPaths(5, edges);
+    cout << noOfPaths;
+
     return 0;
 }
