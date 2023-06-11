310a9e63c1d351e74a3698cd44dcf7469a0245b2
molindu162 <molinduachintha24@gmail.com>
2023-06-11 08:18:56 +0530

    commit

---
+++ SwapNodesAlgo/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+vector<vector<int> > swapNodes(vector<vector<int> > indexes, vector<int> queries) {
+    for (auto h: queries){
+
+    }
+}
+
+
+int main(){
+    return 0;
+}
