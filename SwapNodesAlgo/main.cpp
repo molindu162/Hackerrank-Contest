8c76de4991d8c9d5e76f27a29f2c4fb71aceba7b
molindu162 <molinduachintha24@gmail.com>
2023-06-13 00:24:13 +0530

    commit

--- SwapNodesAlgo/main.cpp
+++ SwapNodesAlgo/main.cpp
@@ -1,15 +1,73 @@
 #include <iostream>
 #include <vector>
+#include <cmath>
 
 using namespace std;
 
-vector<vector<int> > swapNodes(vector<vector<int> > indexes, vector<int> queries) {
-    for (auto h: queries){
+void swapTree(int rootIdx, vector<int> &indexes){
+    int leftChildIdx = rootIdx * 2;
+    int rightChildIdx = leftChildIdx + 1;
+    swap(indexes[leftChildIdx], indexes[rightChildIdx]);
+}
 
+void swapNodes(vector<int> &indexes, vector<int> queries) {
+    int height = log2(indexes.size() + 1);
+    for (int h: queries){
+        int ratio = height / h;
+        int count = 1;
+        while (count <= ratio){
+            int heightConsider = h * count;
+            int startRootIdx = pow(2, heightConsider - 1);
+
+            int endRootIdx = startRootIdx + pow(2, heightConsider - 1) - 1;
+
+            for (int i = startRootIdx; i <= endRootIdx; i++){
+                if (indexes[i] != -1){
+                    int leftChildIdx = 4;
+                    int rightChildIdx = 5;
+                    swap(indexes[leftChildIdx], indexes[rightChildIdx]);
+                    swapTree(i, indexes);
+                }
+            }
+
+
+            count++;
+        }
+        for (int j: indexes){
+            cout << j << " ";
+        }
+        cout << endl;
     }
 }
 
 
 int main(){
+    vector<int> indexes;
+    indexes.push_back(0);
+    indexes.push_back(1);
+    indexes.push_back(2);
+    indexes.push_back(3);
+    indexes.push_back(4);
+    indexes.push_back(-1);
+    indexes.push_back(5);
+    indexes.push_back(-1);
+    indexes.push_back(6);
+    indexes.push_back(-1);
+    indexes.push_back(7);
+    indexes.push_back(8);
+    indexes.push_back(-1);
+    indexes.push_back(9);
+    indexes.push_back(-1);
+    indexes.push_back(-1);
+    indexes.push_back(10);
+    indexes.push_back(11);
+
+    vector<int> queries;
+    queries.push_back(2);
+    queries.push_back(4);
+
+    swapNodes(indexes, queries);
+
+
     return 0;
 }
