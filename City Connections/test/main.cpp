39a1c5183c0aa7aae907838d4a9d09a9f2a02c14
molindu162 <molinduachintha24@gmail.com>
2023-06-13 18:25:54 +0530

    refresh

--- City Connections/test/main.cpp
+++ City Connections/test/main.cpp
@@ -3,6 +3,7 @@
 using namespace std;
 
 int main(){
-    cout << "Hello";
+
+    cout << "Molindu";
     return 0;
 }
--- Snakes & Ladders/main.cpp
+++ Snakes & Ladders/main.cpp
@@ -3,6 +3,6 @@
 using namespace std;
 
 int main(){
-    cout << "Hello";
+    cout << "Hi";
     return 0;
 }
